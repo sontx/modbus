cbd1abb7572ffb90df157b48e7d4b206a1cb45b1
sontx <xuanson33bk@gmail.com>
2017-05-26 18:32:19 +0700

    + more simplier to create modbus session

---
+++ Core/ModbusFactory.cs
@@ -0,0 +1,19 @@
+﻿using Modbus.Core.Streams;
+using System.IO.Ports;
+using System.Net.Sockets;
+
+namespace Modbus.Core
+{
+    public static class ModbusFactory
+    {
+        public static IModbusSession CreateRtuSession(SerialPort serialPort)
+        {
+            return new ModbusRtuSession(new ModbusProtocolImpl(new SerialStream(serialPort)));
+        }
+
+        public static IModbusSession CreateTcpSession(TcpClient tcpClient)
+        {
+            return new ModbusTcpSession(new ModbusProtocolImpl(new TcpStream(tcpClient)));
+        }
+    }
+}
