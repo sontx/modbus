3dcba6f38072a801cd3eb26168a3e190097895e0
sontx <xuanson33bk@gmail.com>
2017-05-27 12:01:55 +0700

    + modbus tcp server

---
+++ Core/ModbusTcpServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Modbus.Core
+{
+    public sealed class ModbusTcpServer : IDisposable
+    {
+        private TcpListener _tcpListener;
+
+        public ModbusTcpServer(string address, int port)
+        {
+            var endPoint = new IPEndPoint(string.IsNullOrEmpty(address) ? IPAddress.Any : IPAddress.Parse(address), port);
+            _tcpListener = new TcpListener(endPoint);
+        }
+
+        public Task WaitForConnectionsAsync(Action<IModbusSession> onAcceptAction)
+        {
+            return Task.Run(async () =>
+            {
+                var listner = _tcpListener;
+                if (listner == null || onAcceptAction == null)
+                    return;
+
+                listner.Start();
+                while (true)
+                {
+                    var tcpClient = await listner.AcceptTcpClientAsync();
+                    var modbusSession = ModbusSessionFactory.CreateTcpSession(tcpClient);
+                    onAcceptAction.Invoke(modbusSession);
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            _tcpListener.Stop();
+            _tcpListener = null;
+        }
+    }
+}
